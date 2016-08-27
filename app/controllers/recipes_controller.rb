f8b3534ba0f40f9822a7b89344e204b7cabe5e27
CarinRae <carinlightner@gmail.com>
2016-08-27 08:30:54 +0100

    updated form and listing of recipes by cooking date

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,15 +2,16 @@ class RecipesController < ApplicationController
   before_action :find_recipe,only: [:show,:edit,:update,:destroy]
 
   def index
+    @recipe = Recipe.all.order("cooking_date DESC")
   end
 
   def new
-  	@recipe= Recipe.new
+  	@recipe = Recipe.new
   	#this defines the new recipe function
   end
 
   def create
-  	@recipe= Recipe.new(recipe_params)
+  	@recipe = Recipe.new(recipe_params)
 
     if @recipe.save 
       redirect_to @recipe, notice: "You made a recipe!"
@@ -21,36 +22,26 @@ class RecipesController < ApplicationController
   	#private method only defined in this file
   end
 
-  	def show 	
-  	end
+  def show 	
+  end
 
-  	def edit	
-  	end
+  def edit	
+  end
+ 
 
-  	def update
-  		if @recipe.update(recipe_params)
-  			redirect_to @recipe
-  		else 
-  			render 'edit'
-  		end
-  	end
-  	def destroy
-  		@recipe.destroy
-  		redirect_to root_path
-  	end
+  private
 
-  	private
-
-  	def find_recipe
-  		@recipe=Recipe.find(params[:id])
-  	end
+  def find_recipe
+  	@recipe=Recipe.find(params[:id])
+  end
   		
-  	def recipe_params
-  		params.require(:recipe).permit(:title,:description,:cooking_date)
+  def recipe_params
+  	params.require(:recipe).permit(:title,:description,:cooking_date)
   		#creates the attributes in the recipe 
   		#consider adding in date etc. here
-  	end
   end
+
+end
   	
 
   
